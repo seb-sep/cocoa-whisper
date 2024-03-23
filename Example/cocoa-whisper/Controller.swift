2d9513d2c73e5da981678083039d672842549ceb
seb-sep <59621473+seb-sep@users.noreply.github.com>
2024-03-23 15:59:41 -0400

    init

---
+++ Example/cocoa-whisper/Controller.swift
@@ -0,0 +1,9 @@
+//
+//  Controller.swift
+//  cocoa-whisper_Example
+//
+//  Created by Sebastian  Sepulveda on 3/16/24.
+//  Copyright © 2024 CocoaPods. All rights reserved.
+//
+
+import Foundation
