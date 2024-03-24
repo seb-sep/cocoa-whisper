e24b92b4d3f516e4519239aa54b350e199f3aeeb
seb-sep <59621473+seb-sep@users.noreply.github.com>
2024-03-24 02:55:14 -0400

    comment out fallbackTokenizerConfig and replace with stub

--- build.rb
+++ build.rb
@@ -11,18 +11,18 @@ pod_name = 'cocoa-whisper'
 %x{rm -rf #{pod_name}/Assets #{pod_name}/Classes}
 
 # Pull fresh version of WhisperKit and swift-transformers into pod folder
-# if Dir.exist?("#{pod_name}/WhisperKit")
-#     %x{rm -rf #{pod_name}/WhisperKit}
-# end
-# if Dir.exist?("#{pod_name}/swift-transformers")
-#   %x{rm -rf #{pod_name}/swift-transformers}
-# end
-# Dir.chdir(pod_name) do
-#   whisperkit_repo = "git@github.com:argmaxinc/WhisperKit.git"
-#   tf_repo = "git@github.com:huggingface/swift-transformers.git"
-#   %x{git clone #{whisperkit_repo}}
-#   %x{git clone #{tf_repo}}
-# end
+if Dir.exist?("#{pod_name}/WhisperKit")
+    %x{rm -rf #{pod_name}/WhisperKit}
+end
+if Dir.exist?("#{pod_name}/swift-transformers")
+  %x{rm -rf #{pod_name}/swift-transformers}
+end
+Dir.chdir(pod_name) do
+  whisperkit_repo = "git@github.com:argmaxinc/WhisperKit.git"
+  tf_repo = "git@github.com:huggingface/swift-transformers.git"
+  %x{git clone #{whisperkit_repo}}
+  %x{git clone #{tf_repo}}
+end
 
 
 # Remove all files and folders from current directory path except those in the items array
@@ -77,10 +77,48 @@ end
 hf_imports = ['Hub', 'Tokenizers', 'TensorUtils', 'Generation', 'Models']
 remove_imports(pod_name + '/swift-transformers/Sources/', hf_imports)
 remove_imports(pod_name + '/WhisperKit/Sources/', hf_imports)
+
+# replace fallbackTokenizerConfig with stub
+# need to do this because Bundle.module seems to be undefined for this cocoapods project
+# https://github.com/huggingface/swift-transformers/blob/4f915610451d29a05948802a140880ff37494dad/Sources/Hub/Hub.swift
+# This should be fine, because tokenizers should not be used in a transcription module,
+# and a stub is the most blatant way to mark this as a placeholder
+hub_file = pod_name + '/swift-transformers/Sources/Hub/Hub.swift'
+
+func_stub = <<~STUB
+//\t This is a stubbed out version of the original function; it is not necessary.
+//\t for use with Whisper and the call to Bundle.module.url does not seem to work in Cocoapods
+\tstatic func fallbackTokenizerConfig(for modelType: String) -> Config? {
+\t      return nil
+\t}
+STUB
+
+processed_lines = []
+brace_count = 0
+in_func = false
+content = File.read(hub_file)
+content.each_line do |line|
+  if line.strip.include?('static func fallbackTokenizerConfig')
+    in_func = true
+  end
+  if in_func
+    processed_lines << '// ' + line
+    brace_count += line.count('{') - line.count('}')
+    if brace_count == 0
+      in_func = false
+      processed_lines << func_stub
+    end
+  else
+    processed_lines << line
+  end
+end
+
+File.write(hub_file, processed_lines.join)
+
+
 # open project
 proj_path = './_Pods.xcodeproj'
 project = Xcodeproj::Project.open(proj_path)
-
 
 # Target to add files to
 target = project.targets.first
